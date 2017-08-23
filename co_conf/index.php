c0eac20540c36bdad0e6f3142bc38b50a9a6baf3
Lluis Bosch <lbosch@icra.cat>
2017-08-23 20:12:46 +0200

    implementing construction

---
+++ co_conf/index.php
@@ -0,0 +1,169 @@
+<!doctype html><html><head>
+	<meta charset=utf-8>
+	<meta name="viewport" content="width=device-width">
+	<meta name="description" content="ecoinvent">
+	<title>Construction</title>
+	<style>
+		div[id]{
+			border-bottom:1px solid #ccc;
+			padding-bottom:5px;
+			margin-bottom:5px;
+		}
+	</style>
+</head><body>
+
+<h1>Construction</h1>
+
+<div>
+	Capacity of the plant (m<sup>3</sup>/d)
+	<input value=0>
+</div>
+
+<!--
+<div class=tab_buttons>
+	<style>
+		/*tab buttons*/
+			div.tab_buttons               { padding:0.5em 0;display:flex;justify-content:center}
+			div.tab_buttons button        { display:block;padding:6px 10px;border:1px solid #ccc;background:#f5f5f5;outline:none;}
+			div.tab_buttons button:hover  { background:#e6e6e6}
+			div.tab_buttons button.left   { border-radius:0.5em 0.0em 0.0em 0.5em; border-right-style:none}
+			div.tab_buttons button.right  { border-radius:0.0em 0.5em 0.5em 0.0em; }
+			div.tab_buttons button.middle { border-right-style:none}
+			div.tab_buttons button.single { border-radius:0.5em}
+			div.tab_buttons button[disabled] { background-color:#ccc;box-shadow:inset 0 2px 4px rgba(0,0,0,.15),0 1px 2px rgba(0,0,0,.05);}
+		/*tab buttons*/
+	</style>
+	<button class=left>Primary</button>
+	<button class=middle>Secondary</button>
+	<button class=right>Tertiary</button>
+</div>
+-->
+
+<div>
+	<h3>1. Construction</h3>
+	<table>
+		<tr>
+			<th>Material
+			<th>Pretreatment
+			<th>Secondary
+			<th>Sludge
+			<th>Tubes
+		</tr>
+	</table>
+</div>
+Material/Unit
+ Pretreatment
+  Secondary treatment
+	 Sludge line
+	  Tube connections
+		Diesel burned in
+		 Eq.
+		  1.74·101x
+			 1.59·102x+1.39·105
+			  8.76x+1.57·103
+				 2.22·101x+1.20·10 5
+				 2
+				 machines (MJ)
+				  R 1
+					 0.999
+					  0.991
+						 0.953
+						 Soil excavation
+						  Eq.
+							 1.16·102
+							 x
+							  1.06·103
+								x+9.30·10
+								5
+								 5.76·101
+								 x
+								  0 t 1.51·103
+									(kg)
+									 R2
+									  1
+										 0.999
+										  0.967
+											Soil transport to
+											 Eq.
+											  4.65x
+												 4.24·101
+												 x+3.72·10
+												 4
+												  2.31x
+													 0 to 6.04·101
+													 landfill (tkm)
+													  R2
+														 1
+														  0.999
+															 0.967
+															 Concrete (kg)
+															  Eq.
+																 6.52·101
+																 x+4.39·10
+																 4
+																  1.11·103
+																	x-2.98·10
+																	5
+																	 9.27·101x-6.56·10 3
+																	  3.61·10-5 to 3.64·101
+																		R2
+																		 0.901
+																		  0.997
+																			 0.960
+																			 2
+																			  1
+																				 4
+																				  3
+																					Reinforcing
+																					 Eq.
+																					  2.54x-5.74·10
+																						 4.27·10 x-2.41·10
+																						  2.93x-2.29·10 0 to 1.40
+																							2
+																							steel (kg)
+																							 R
+																							  0.886
+																								 0.971
+																								  0.873
+																									Metals
+																									 for
+																									  Eq.
+																										 9.14·10-3
+																										 x+4.25·10
+																										 1
+																										  1.65·10-1
+																											x+1.05·10
+																											2
+																											 1.01·10-2x-7.01
+																											  0 to 2.48·10-3
+																												formworks (kg)
+																												 R2
+																												  0.766
+																													 0.944
+																													  0.733
+																														Other
+																														 metals
+																														  Eq.
+																															 4.26·10-2
+																															  to 3.12·10
+																																-1
+																																 3.13·10-2
+																																  to 2.31·10
+																																	-1
+																																	 5.20·10-2 to 3.02
+																																	  1.62·10-1 to 3.60
+																																		(kg)
+																																		Plastics
+																																		 for
+																																		  Eq.
+																																			 3.94·10-4x+1.83
+																																			  7.11·10-3x+4.54
+																																				 4.29·10-4x-3.11·10 -1
+																																				  0 to 1.07·10-4
+
+
+
+<hr>
+<div>
+	<button>Save</button>
+</div>
