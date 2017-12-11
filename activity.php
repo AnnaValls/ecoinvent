00e37ed9379c17a3f062028001a2c021980b9fb7
root <root@debian.icra>
2017-12-11 17:42:49 +0100

    update activity

--- activity.php
+++ activity.php
@@ -1,6 +1,5 @@
 <?php
 /* 
- *
  * Description of this php page
  *
  */
@@ -13,6 +12,8 @@
 <?php include'navbar.php'?>
 <div id=root>
 <h1>Create an Activity (wastewater generation)</h1><hr>
+
+<!--status-->
 <h4>status: developing user interface (before backend)</h4>
 
 <!--create activity menu-->
@@ -21,6 +22,7 @@
     <td>Name 
     <td><input placeholder="Name">
 
+    <!--user chooses reference data here--> 
     <td rowspan=3 style=text-align:center>
       Compare activity vs reference data: 
       <select>
@@ -45,42 +47,71 @@
     <td>Activity
     <td>Steel, RER <issue class="help_wanted"></issue>
   <tr>
-    <td colspan=2>
+    <td colspan=2 style="vertical-align:top">
       <p>Define wastewater characteristics of your activity</p>
       <table id=inputs></table>
       <!--inputs.row template-->
       <template id=inputs_row>
-        <tr>
+        <tr title class=help>
           <td id>
           <td><input id value>
           <td unit style=font-size:smaller>
         </tr>
       </template>
     </td>
-    <td>
+    <td style="vertical-align:top">
       <p>Chosen Reference data wastewater characteristics</p>
       <table id=inputs_reference></table>
     </td>
     <td style=vertical-align:top>
-      Outputs here
+      Outputs will appear here
       after clicking 'Compare'
     </td>
 </table>
-
 
 <script>
   //fx: frontend
   (function populate_inputs(){
     populate_table('inputs');
     populate_table('inputs_reference',true);
-    function populate_table(id,disabled){
+    populate_table('inputs_reference',true,true);
+
+    function populate_table(id,disabled,design_parameters){
       disabled=disabled||false;
+      design_parameters=design_parameters||false;
+
+      //get table by id
       var t=document.getElementById(id);
-      while(t.rows.length>0)t.deleteRow(-1);
+
+      //empty table
+      if(!design_parameters) while(t.rows.length>0)t.deleteRow(-1);
+
+      //get template
       var template=document.querySelector("template#inputs_row");
-      Inputs.filter(inp=>{return !inp.isParameter}).forEach(inp=>{
+
+      //filter inputs depending on if we want design parameters or normal inputs
+      var filtered_Inputs;
+      if(!design_parameters){
+        filtered_Inputs=Inputs.filter(inp=>{return !inp.isParameter});
+      }else{
+        filtered_Inputs=Inputs.filter(inp=>{return inp.isParameter});
+      }
+
+      //add a mini header for design parameters
+      if(design_parameters){
+        var newRow=t.insertRow(-1);
+        newRow.insertCell(-1).outerHTML="<td colspan=3><p><b>Design parameters</b></p>";
+      }
+
+      //go over Inputs
+      filtered_Inputs.forEach(inp=>{
         //clone row template
         var clone=template.content.cloneNode(true);
+
+        //title (caption)
+        var tr=clone.querySelector('tr');
+        tr.title=inp.descr;
+
         //name
         var td=clone.querySelector('td[id]');
         td.innerHTML=inp.id;
@@ -98,3 +129,4 @@
     }
   })();
 </script>
+
