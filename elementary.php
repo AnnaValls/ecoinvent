30cf4da9d998edc30c99688986968bf34c77001d
holalluis <holalluis@gmail.com>
2017-12-21 00:25:08 +0100

    added a button to clear cache

--- elementary.php
+++ elementary.php
@@ -548,4 +548,13 @@
     <li>TS (Sulfur) effluent unknown.  <issue class="help_wanted"></issue>
     <li>S (Sulfur) mass balance equations unknown.  <issue class="help_wanted"></issue>
   </ul>
+
+  <hr>
+
+  <p>
+    <b>Note</b>:
+    If the tool has been updated recently it may not work due to the browser keeping development files in a place called "the cache". 
+    To make sure you flush the old files and have the latest ones, click here:<br>
+    <button onclick="window.location.reload(true)">Clear cache</button>
+  </p>
 </div></p>
