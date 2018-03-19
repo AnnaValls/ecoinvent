2a58dd700877ec5f645f81887ecb9cae916e57b6
holalluis <holalluis@gmail.com>
2018-03-19 22:44:35 +0100

    python server side working

--- ecospold/index.php
+++ ecospold/index.php
@@ -1,24 +1,23 @@
 <?php
-  $cmd=isset($_GET['cmd']) ? $_GET['cmd'] : 'python test.py argument';
+  $cmd=isset($_GET['cmd']) ? $_GET['cmd'] : 'python test.py a b c d';
+  $cmd=escapeshellcmd($cmd);
 ?>
 
-<!--input command here-->
+<!--shell prompt-->
 <form method=GET>
-  <input name=cmd id=cmd value="<?php echo $cmd?>" placeholder="write command here" style="width:100%">
+<input name=cmd id=cmd value="<?php echo $cmd?>" placeholder="write command here" style="width:50%">
 </form>
 
-<!--show input-->
+<!--input-->
 <?php echo "<h2><code>&gt; $cmd</code></h2>"?>
 
-<!--show output-->
-<pre>&gt;<code>
-<?php
-  $result=passthru($cmd);
+<!--output-->
+<pre><code>
+<?php 
+  $result=shell_exec($cmd); 
   echo $result;
 ?>
-<pre><code>
+</pre></code>
 
 <!--focus on cmd prompt-->
-<script>
-  document.querySelector('#cmd').select();
-</script>
+<script>document.querySelector('#cmd').select();</script>
--- ecospold/test.py
+++ ecospold/test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import random
 import sys
 
@@ -5,11 +6,11 @@ import sys
 print("This is a dummy <a href='test.py'>python script</a> output executed on the server\n")
 
 #compute a random number
-print("Random number {0, 10000}:",int(10000*random.random()),'\n')
+print("Random number {0, 10000}: "+str(int(10000*random.random()))+'\n')
 
 #print argument list
 print("Argument list:")
 i=0;
 for arg in sys.argv:
-  print("-",i,arg)
+  print("- "+str(i)+" "+arg)
   i+=1;
