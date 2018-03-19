53249e287d49aba1197d5a9ecf058bbb48fe1793
holalluis <holalluis@gmail.com>
2018-03-19 21:59:30 +0100

    corrected syntax errors for python 3

--- ecospold/test.py
+++ ecospold/test.py
@@ -2,14 +2,14 @@ import random
 import sys
 
 #hello
-print "This is a dummy <a href='test.py'>python script</a> output executed on the server\n"
+print("This is a dummy <a href='test.py'>python script</a> output executed on the server\n")
 
 #compute a random number
-print "Random number {0, 10000}:",int(10000*random.random())
+print("Random number {0, 10000}:",int(10000*random.random()),'\n')
 
 #print argument list
-print "Argument list:"
+print("Argument list:")
 i=0;
 for arg in sys.argv:
-  print "-",i,arg
+  print("-",i,arg)
   i+=1;
