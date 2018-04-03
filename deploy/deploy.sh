5a0b1d9ce7544f78fb4e8088d118e7ba73a8f3db
holalluis <holalluis@gmail.com>
2018-04-04 00:43:34 +0200

    deploying faster

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-#automatic git pull in webapps icra
-curl "http://84.89.61.64:8030/ecoinvent/ecospold/" \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "input=git+pull+%26%26+cd+wastewater_treatment_tool+%26%26+git+pull"
+#git pull in webapps icra (windows)
+curl "http://84.89.61.64:8030/ecoinvent/gitpull.php"
 
-#pull in ecoinvent server
+#git pull in ecoinvent server (linux)
 ssh prqv_wastewater@prqv.ftp.infomaniak.com 'cd web/ecoinvent; git pull; cd ecospold/wastewater_treatment_tool; git pull'
