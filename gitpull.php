2ddb32d22617e6db4800e9750b60ddc77fd2a10b
holalluis <holalluis@gmail.com>
2018-04-04 01:03:32 +0200

    ecospold generation testing

--- gitpull.php
+++ gitpull.php
@@ -1,4 +1,5 @@
 <?php
   echo "icra server (windows) git pull: \n";
-  var_dump(shell_exec("git pull & cd wastewater_treatment_tool & git pull"));
+  #var_dump(shell_exec("git pull & cd wastewater_treatment_tool & git pull"));
+  var_dump(shell_exec("git pull"));
 ?>
