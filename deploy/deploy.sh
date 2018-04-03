341b07858a2bb3c99b7b027ac53963671ea358fc
holalluis <holalluis@gmail.com>
2018-04-04 00:40:44 +0200

    testing ecospold generation

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#automatic git pull
+#automatic git pull in webapps icra
 curl "http://84.89.61.64:8030/ecoinvent/ecospold/" \
   -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "input=git+pull+%26+cd+wastewater_treatment_tool+%26+git+pull"
+  -d "input=git+pull+%26%26+cd+wastewater_treatment_tool+%26%26+git+pull"
 
-#pull in the ecoinvent server
+#pull in ecoinvent server
 ssh prqv_wastewater@prqv.ftp.infomaniak.com 'cd web/ecoinvent; git pull; cd ecospold/wastewater_treatment_tool; git pull'
