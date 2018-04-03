864911c983a955cac1ca370c95505262c3bcc126
holalluis <holalluis@gmail.com>
2018-04-04 01:42:35 +0200

    ecospold generation testing

--- generate_untreated_ecospold.php
+++ generate_untreated_ecospold.php
@@ -18,10 +18,24 @@
 
   <script>
     function genereate_untreated_ecospold(){
-      //windows
-      //post('ecospold/index.php',"python generate_untreated_ecospold.py '"+encodeURI(JSON.stringify(received_json))+"'");
-      //unix
-      post('ecospold/index.php',"./generate_untreated_ecospold.py '"+JSON.stringify(received_json)+"'");
+      //detect OSname
+      var OSName = "Unknown";
+      if(window.navigator.userAgent.indexOf("Windows NT 10.0")!= -1) OSName="Windows 10";
+      if(window.navigator.userAgent.indexOf("Windows NT 6.2") != -1) OSName="Windows 8";
+      if(window.navigator.userAgent.indexOf("Windows NT 6.1") != -1) OSName="Windows 7";
+      if(window.navigator.userAgent.indexOf("Windows NT 6.0") != -1) OSName="Windows Vista";
+      if(window.navigator.userAgent.indexOf("Windows NT 5.1") != -1) OSName="Windows XP";
+      if(window.navigator.userAgent.indexOf("Windows NT 5.0") != -1) OSName="Windows 2000";
+      if(window.navigator.userAgent.indexOf("Mac")            != -1) OSName="Mac/iOS";
+      if(window.navigator.userAgent.indexOf("X11")            != -1) OSName="UNIX";
+      if(window.navigator.userAgent.indexOf("Linux")          != -1) OSName="Linux";
+
+      //if windows...
+      if(OSName.includes("Windows")){
+        post('ecospold/index.php',"python generate_untreated_ecospold.py '"+JSON.stringify(received_json)+"'");
+      }else{
+        post('ecospold/index.php',     "./generate_untreated_ecospold.py '"+JSON.stringify(received_json)+"'");
+      }
     }
   </script>
 </div>
