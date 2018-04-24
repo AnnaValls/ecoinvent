5a1add95722ea2737fdb7f0f1663616eb7b832ff
holalluis <holalluis@gmail.com>
2018-04-24 22:59:55 +0200

    gitpull automatic

--- gitpull.php
+++ gitpull.php
@@ -1,4 +1,4 @@
 <?php
   //var_dump(shell_exec("pwd; git pull 2>&1"));
-  var_dump(shell_exec("cd ecospold/wastewater_treatment_tool/; pwd; git pull 2>&1"));
+  var_dump(shell_exec("cd ecospold/wastewater_treatment_tool/; git pull 2>&1"));
 ?>
